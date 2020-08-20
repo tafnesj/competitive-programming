c68e51dabaaf6c9d280d55ca0c34dd256be67093
tafnesj <tafnesj@users.noreply.github.com>
2020-08-20 00:07:37 -0500

    Add solution

---
+++ Codeforces/girlsChallenge2/C - Learning to write.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+  string word;
+  cin >> word;
+
+  for(int i=0; i<word.size(); i++){
+    if(word[i]>= 'a' && word[i] <= 'z'){
+      word[0] = toupper(word[0]);
+    }
+  }
+
+  cout << word;  
+  return 0;
+}
---
+++ Codeforces/girlsChallenge2/D - Oscar is in trouble.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int size=0; 
+  string code;
+  cin >> code;
+    
+  size = code.size();
+    
+  for(int i=0; i<size; i++){
+    if(code[i]=='.'){
+      cout << "0";
+    }else if(code[i]=='-' && code[i+1]=='.'){
+      cout <<"1";
+      i+=1;
+    }else if(code[i]=='-' && code[i+1]=='-'){
+      cout <<"2";
+      i+=1;
+    }
+  }
+
+  return 0;
+}
